be2a1871c75c19f3dcec03ae1afd7c74c249c0d0
birdybro <aberusugi@gmail.com>
2022-11-13 15:22:14 -0700

    m57: Invert red color bits to fix ocean color

--- src/mame/irem/m57_v.cpp
+++ src/mame/irem/m57_v.cpp
@@ -43,8 +43,8 @@ void m57_state::m57_palette(palette_device &palette) const
 
 		// red component
 		bit0 = 0;
-		bit1 = BIT(color_prom[256], 2);
-		bit2 = BIT(color_prom[256], 3);
+		bit1 = BIT(color_prom[256], 3);
+		bit2 = BIT(color_prom[256], 2);
 		int const r = 0x21 * bit0 + 0x47 * bit1 + 0x97 * bit2;
 		// green component
 		bit0 = BIT(color_prom[0], 3);
